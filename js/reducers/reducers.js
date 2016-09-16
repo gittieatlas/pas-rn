f977a2538de7d19f1f87d5f54ddf3f85c42b2b7c
Gittie Atlas <gittie@1on1development.com>
2016-09-15 22:27:45 -0400

    navigation and screens

---
+++ js/reducers/reducers.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux';
+import * as types from '../actions/actionTypes';
+import * as NavigationStateUtils from 'NavigationStateUtils';
+
+const initialNavState = {
+	index: 0,
+	routes: [
+		{ key: 'First', title: 'First' }
+	]
+};
+
+function navigationState(state = initialNavState, action) {
+	switch (action.type) {
+	case types.NAV_PUSH:
+		if (state.routes[state.index].key === (action.state && action.state.key)) return state;
+		return NavigationStateUtils.push(state, action.state);
+
+	case types.NAV_POP:
+		if (state.index === 0 || state.routes.length === 1) return state;
+		return NavigationStateUtils.pop(state);
+
+	case types.NAV_JUMP_TO_KEY:
+		return NavigationStateUtils.jumpTo(state, action.key);
+
+	case types.NAV_JUMP_TO_INDEX:
+		return NavigationStateUtils.jumpToIndex(state, action.index);
+
+	case types.NAV_RESET:
+		return {
+			...state,
+			index: action.index,
+			routes: action.routes
+		};
+
+	default:
+		return state
+	}
+}
+
+const appReducers = combineReducers({
+	navigationState
+});
+
+export default appReducers
